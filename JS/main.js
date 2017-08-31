9d7f51839c545df96214d3dc4fb0e675da2e37b2
tinyfatboy <tinyfatboy@outlook.com>
2017-09-01 01:58:18 +0800

    change password logo when focus

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,7 @@
+$("form input[name='password']").on('focus', function () {
+    $('img').attr('src', 'img/22.jpg').css('border', '1px solid #D80053')
+})
 
+$("form input[name='password']").on('blur', function () {
+    $('img').attr('src', 'img/2233x.jpg').css('border', 'none')
+})
